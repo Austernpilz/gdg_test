058523ebc3d0826e181031df4314865a1ad1d581
Manuel Zschaebitz <manuez42@zedat.fu-berlin.de>
2024-12-02 14:10:12 +0100

    wip

--- test/main.cpp
+++ test/main.cpp
@@ -6,8 +6,11 @@ int main(int argc, char *argv[])
   gdg dna;
   for (uint8_t i = 1; i<argc; ++i)
   {
+    
     std::string ma = argv[i];
     std::string s = argv[i+1];
+
+    std::cout << ma << " " << s;
     if (ma == "-ref")
     {dna.set_ref_genom(s);}
     if (ma == "-s")
